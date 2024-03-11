97d64b6947fa27cb2db472d02b07443e391a16fa
Lionsgazing <43805956+Lionsgazing@users.noreply.github.com>
2024-03-11 10:17:49 +0100

    Update example

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import json
 import secrets
+from time import sleep
 from flask import Flask, request, make_response, redirect, url_for
+import asyncio
 
 app = Flask(__name__)
 
@@ -8,12 +10,18 @@ app = Flask(__name__)
 def index():
     """Main page example with the different methods. Probably only need the 'GET' method for this."""
     if request.method == "GET":
+        # Get example
         response = make_response('<p> Main page </p>')
 
+        # Read data from csv and return it to the user.
+
+
     if request.method == "POST":
+        # Post example
         pass
 
     if request.method == "DELETE":
+        # Delete example
         pass
 
     return response
